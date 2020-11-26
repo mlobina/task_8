3b5913a3742b1f74d5bf2bc50a69db84d4e8771e
Marina Lobina <mvlobina@gmsil.com>
2020-11-27 01:14:38 +0300

    task_8.1, 8.2

--- task_8.1.py
+++ task_8.1.py
@@ -1,3 +1,4 @@
+
 cook_book_dict = {}
 keys = ['ingredient_name', 'quantity', 'measure']  # список ключей для каждого ингредиента
 
@@ -22,4 +23,34 @@ with open('cook_book.txt') as text:
             ingr_dict = {k: v for (k, v) in ingr_zp}  # создаем dict для текущего ингредиента
             cook_book_dict[dish].append(ingr_dict)  # собираем словари ингредиентов в общий словарь
         continue  # переходим к следующему dish
-    print(cook_book_dict)
+
+
+
+    def get_shop_list_by_dishes(dishes, person_count):
+        shop_list_by_dishes = {}
+        ingredient = {}
+
+        for dish in dishes:
+
+            for key in cook_book_dict.keys():
+
+                if key == dish:
+                    ingr_list = cook_book_dict[dish]
+
+                    for value in ingr_list:
+                        name = value['ingredient_name']
+                        measure = value['measure']
+                        quantity = int(value['quantity']) * person_count
+                        ingredient = {'measure': measure, 'quantity': quantity}
+
+                        if name not in shop_list_by_dishes.keys():
+                            shop_list_by_dishes[name] = ingredient
+                        else:
+                            shop_list_by_dishes[name]['quantity'] += ingredient['quantity']
+
+        print(shop_list_by_dishes)
+
+    get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
